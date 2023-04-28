61fee3c1359e3b583b8c576f9fae23917128548e
Ormendel <orimendel@gmail.com>
2023-04-28 15:00:47 +0300

    Creating a small interface for all algorithms

---
+++ Algorithm.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public interface Algorithm
+{
+    private static void setCost_to_goal(Node n){};
+    private static void write_outputFile() throws IOException{};
+
+    public static void run(Node start) throws IOException{};
+}
