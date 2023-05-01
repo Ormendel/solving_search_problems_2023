dfd59adc32c4031ffa71968912de5eb9018e76a4
Ormendel <orimendel@gmail.com>
2023-05-01 09:07:00 +0300

    removing setCost_to_goal function, we don't need it

--- Algorithm.java
+++ Algorithm.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 
 public interface Algorithm
 {
-    private static void setCost_to_goal(Node n){};
     private static void write_outputFile() throws IOException{};
 
     public static void run(Node start) throws IOException{};
