d6adb16f0bd04812d89a1cbfa18338802f37cd17
Ormendel <orimendel@gmail.com>
2023-05-02 15:11:49 +0300

    DFID needs to be fixed (infinity loop)

--- DFID.java
+++ DFID.java
@@ -1,4 +1,116 @@
-public class DFID
-{
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class DFID extends Ex1 implements Algorithm
+{
+    private static Hashtable<String, Node> open_list = new Hashtable<>();
+    private static Hashtable<String, Node> closed_list = new Hashtable<>();
+    private static int iteration = 1;
+    private static void write_outputFile() throws IOException
+    {
+        File file = new File("outputFiles/my_output_for_DFID.txt");
+        FileWriter fw = new FileWriter(file,true);
+        PrintWriter pw = new PrintWriter(fw);
+
+        if(path.isEmpty())
+        {
+            pw.println("no path");
+            pw.println("Num: "+created_states);
+            pw.println("Cost: inf");
+        }
+        else
+        {
+            pw.println(path);
+            pw.println("Num: "+created_states);
+            pw.println("Cost: "+cost);
+        }
+        pw.print(seconds+" seconds");
+        pw.close();
+    }
+
+    public static void run(Node start) throws IOException
+    {
+        startTime = System.currentTimeMillis();
+        // iterate until we will find goal or no where to go
+        for (int depth = 1; depth < Integer.MAX_VALUE; depth++)
+        {
+            HashMap<String, Node> h = new HashMap<>();
+            open_list.clear();closed_list.clear();
+            String result = limited_DFS(start, depth, h);
+            if (!result.equals("cutoff")) /* if we found a solution to problem return the path to goal, otherwise return no path */
+            {
+                long end = System.currentTimeMillis() - startTime;
+                seconds = end / 1000.0;
+            }
+        }
+        endTime = System.currentTimeMillis() - startTime;
+        seconds = endTime / 1000.0;
+        System.out.println("path from start to goal: "+path);
+        System.out.println("Num: "+created_states); // Number of states is set
+        System.out.println("Cost: "+cost);
+        System.out.println(seconds+" seconds"); // seconds is set
+    }
+
+
+    private static String limited_DFS(Node n, int limit, HashMap<String, Node> h)
+    {
+        if(var.with_open)
+        {
+            System.out.println("\n==========ITERATION #"+(iteration++)+": LIMIT #"+(limit)+" ==========");
+            System.out.println(open_list);
+        }
+        //check if we found a solution
+        if (n.getTag() == 'G')
+        {
+            cost = n.getCost(); // cost is set
+            path = n.getPath().substring(0,n.getPath().length()-1); // path is set
+            if(var.with_open)
+                System.out.println("\n Goal was found =] \n");
+            return path;
+        }
+
+        // else if limit reached zero we will go back and look deeper in the next round
+        else if (limit == 0)
+            return "cutoff";
+        else
+        {
+            open_list.put(n.getSearchedKey(), n);
+            h.put(n.getSearchedKey(), n); /* insert Hash table current node */
+
+            boolean isCutoff = false;
+            Operator op = new Operator();
+            op.setN(n);
+            Queue<Node> children= op.operator(op.getN());
+            System.out.println(children);
+            //for each node in children - check
+            for (Node g : children)
+            {
+                ++created_states;
+                if (h.containsKey(g.getSearchedKey()))
+                 /* if hash table contains that path then continue to next operator */
+                    continue;
+                String result = limited_DFS(g, limit - 1, h); /* recurse to deeper layer with the current node */
+
+                if (result.equals("cutoff")) /* if the string equals to cutoff then isCutoff will also will set to true */
+                    isCutoff = true;
+                else if (!result.equals("fail")) /* if the result doesn't equal to fail return the result */
+                    return result;
+            }
+            closed_list.put(n.getSearchedKey(), n); //finished iterating over all the children, so add to closed list
+            //if we reached here means we couldn't find solution with n node so will remove it from hash table
+            h.remove(n.getSearchedKey(),n);
+            open_list.remove(n.getSearchedKey(),n);
+            //if cutoff true -> return cutoff
+            if (isCutoff)
+                return "cutoff";
+            // else return fail
+            return "fail";
+        }
+    }
 }
